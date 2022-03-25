d4e0abda86a255b7ede21601a822d5662de31d42
uzunb <batuhanuzun_@outlook.com>
2022-03-26 01:02:17 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const electon = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electon;
+
+let mainWindow;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'main.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
