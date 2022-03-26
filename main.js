1a8c42adad0511af18fcdd9d5c502079dca8cf57
uzunb <batuhanuzun_@outlook.com>
2022-03-26 13:38:30 +0300

    Add new task function was added

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electon = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electon;
+const {app, BrowserWindow, Menu} = electon;
 
 let mainWindow;
 
@@ -13,4 +13,52 @@ app.on('ready', function(){
         protocol: 'file:',
         slashes: true
     }));
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
 });
+
+// Handle create add task window
+function createAddTaskWindow(){
+    addTaskWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Task'
+    });
+
+    addTaskWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addTask.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    addTaskWindow.on('close', function(){
+        addTaskWindow = null;
+    });
+}
+
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                click(){
+                    createAddTaskWindow();
+                }
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }                                                                                                                                                             
+        ]
+    }
+];
